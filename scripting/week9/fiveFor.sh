55362bedb2cfa74e2b7281a7ef458704df0094d5
rmit-s3547126-Ashley-Rowland <s3547126@student.rmit.edu.au>
2018-10-09 16:52:37 +1100

    Week9 scripting

---
+++ scripting/week9/fiveFor.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+high=0
+low=9999
+counter=0
+
+for i in $@ ; do
+
+  if [ -z $i ] ; then
+    break
+  fi
+
+  if [ $i -lt $low ] ; then
+    low=$i
+  fi
+
+  if [ $i -gt $high ] ; then
+    high=$i
+  fi
+
+  shift
+  counter=$((counter+1))
+done
+
+echo "Lowest value supplied was: $low"
+echo "Highest value supplied was: $high"
---
+++ scripting/week9/fiveWhile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+
+HIGH=0
+LOW=9999
+counter=0
+
+while [ -n $1 -a $counter -lt 5 ] ; do
+
+  if [ $1 -lt $LOW ] ; then
+    LOW=$1
+  fi
+
+  if [ $1 -gt $HIGH ] ; then
+    HIGH=$1
+  fi
+
+  shift
+  counter=$((counter+1))
+
+done
+
+echo "Lowest value supplied was: $LOW"
+echo "Highest value supplied was: $HIGH"
