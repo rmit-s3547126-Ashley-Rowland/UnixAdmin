7a7c56f0ea926f4e6f2bb541acef71b83c556db3
rmit-s3547126-Ashley-Rowland <s3547126@student.rmit.edu.au>
2018-10-09 15:14:44 +1100

    getopts script

---
+++ scripting/go_opt.sh
@@ -0,0 +1,11 @@
+while getopts ":a" opt; do
+  case $opt in
+       a)
+         echo "-a was triggered!" >&2
+         ;;
+       \?)
+         echo "Invalid option: -$OPTARG" >&2
+         ;;
+  esac
+done
+
